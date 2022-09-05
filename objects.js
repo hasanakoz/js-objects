0ed506ec4e5849e50c3f6ba7187589544911fa20
hasan <hasanakoz1986@gmail.com>
2022-09-05 21:23:42 +0300

    two methods of creating objects

--- objects.js
+++ objects.js
@@ -10,3 +10,34 @@ const sumOfRaisedSalaries = salaries
   .map((sal) => Math.trunc(sal * 1.1))
   .reduce((acc, salary) => acc + salary, 0);
 console.log(sumOfRaisedSalaries);
+
+//******************************************************************OBJECTS**************** */
+
+// 1st method to create an object
+const cars = new Object();
+cars.brand = "BMW";
+cars.engine = "1.3";
+cars.model = 2022;
+cars.lpg = true;
+console.log(cars);
+console.log(cars.brand); //.dot notation
+console.log(cars["engine"]); //square bracket notation
+const key = "model";
+console.log(cars[key]); //square bracket notation allows to define and invoke with a varible.
+
+cars.engine = "1.6"; //change the value
+console.log(cars);
+
+//& 2nd method to create object OBJECT CONSTRUCTOR
+
+function Personel(id, ad, maas) {
+  this.perId = id;
+  this.perAd = ad;
+  this.maas = maas;
+}
+const kisi1 = new Personel("1245", "mustafa", 15000);
+const kisi2 = new Personel("1654", "Canan", 20000);
+
+console.log(kisi1);
+console.log(kisi2.perAd);
+console.log(kisi2["perId"]);
